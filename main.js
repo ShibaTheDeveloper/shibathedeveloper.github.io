2e2e5ebb595b7f29bde9bf5d98b6e46635b1da71
shiba1212 <shibayt4@gmail.com>
2025-03-14 21:16:29 +0100

    Update main.js

--- main.js
+++ main.js
@@ -131,7 +131,7 @@ const PAGES = {
 
 var higher = `
     <h2>QUOTE OF THE DAY: "<span id="quote-text">Loading...</span>" <span id="quote-author">Loading...</span></h2>
-    <br><br>
+    <br>
 
     <div class="button-container">
         <button onclick="loadPage('index')">home</button>
