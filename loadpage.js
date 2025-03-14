f053f4ad043f36600ccb714c3a50396e6250ec68
shiba1212 <shibayt4@gmail.com>
2025-03-14 15:32:43 +0100

    t

--- loadpage.js
+++ loadpage.js
@@ -3,68 +3,55 @@ const PAGES = {
         <h1>welcome!</h1>
 
         <p>
-            i'm shiba, a game dev from poland. <br><br>
-            i mainly work with luau in roblox studio, but i'm also trying to learn python and c#. <br><br>
-            if you want to contact or check out some of my works, check the buttons below.
+            im shiba, a game dev from poland mainly working with luau in roblox studio since 2018. <br>
+            im always trying to improve my coding skills and learn new languages. <br>
+            you can check out my projects, or contact me using the buttons above.
         </p>
-
-       <div class="button-container">
-            <button onclick="loadPage('socials')">socials</button>
-            <button onclick="loadPage('mywork')">my works</button>
-        </div>
-
-        <small>
-            <br>
-            the favicon is a picutre of sunny from omori
-        </small>
     `,
     mywork: `
         <h1>my works</h1>
 
+        <img src="Assets/embed.png></img>
         <p>
-            <a href="https://www.roblox.com/games/16634562549/TBB-TARPG-REWORK-OPEN-ALPHA">TBB:TARPG</a>
+            <a href="https://www.roblox.com/games/16634562549/TBB-TARPG-REWORK-OPEN-ALPHA">TBB:TARPG</a> <br>
             a fan RP game of <a href="https://www.roblox.com/games/10834586502/The-Battle-Bricks">Tumore's The Battle Bricks</a>, my first ever successful project
         </p>
 
+        <img src="Assets/embed.png></img>
         <p>
-            <a href="https://www.roblox.com/games/88623920231246/Teapot-Tumble">Teapot Tumble</a>
+            <a href="https://www.roblox.com/games/88623920231246/Teapot-Tumble">Teapot Tumble</a> <br>
             a spin on roblox's classic falling teapots of doom game by clockwork
         </p>
-
-       <div class="button-container">
-            <button onclick="loadPage('index')">back</button>
-            <button onclick="loadPage('socials')">socials</button>
-        </div>
-
-        <small>
-            <br>
-            the favicon is a picutre of sunny from omori
-        </small>
     `,
     socials: `
         <h1>socials</h1>
 
         <p>
-            discord: shibahh <br><br>
-            roblox: <a href="https://www.roblox.com/users/2456801075/profile" target="_blank">ssshibah</a> (uid: 2456801075) <br><br>
-            yt: <a href="https://www.youtube.com/@shibahhh" target="_blank">@shibahhh</a> <br><br>
-            github: <a href="https://github.com/shibahh" target="_blank">shibahh</a> <br><br>
+            discord: shibahh <br>
+            roblox: <a href="https://www.roblox.com/users/2456801075/profile" target="_blank">ssshibah</a> (uid: 2456801075) <br>
+            yt: <a href="https://www.youtube.com/@shibahhh" target="_blank">@shibahhh</a> <br>
+            github: <a href="https://github.com/shibahh" target="_blank">shibahh</a> <br>
         </p>
-        
-        <div class="button-container">
-            <button onclick="loadPage('index')">back</button>
-            <button onclick="loadPage('mywork')">my works</button>
-        </div>
-
-        <small>
-            <br>
-            the favicon is a picutre of sunny from omori
-        </small>
     `
 }
 
+var buttons = `
+    <div class="button-container">
+        <button onclick="loadPage('index')">home</button>
+        <button onclick="loadPage('socials')">socials</button>
+        <button onclick="loadPage('mywork')">my works</button>
+    </div>
+`
+
+var small = `
+    <small>
+        <br>
+         the favicon is a picutre of sunny from omori
+    </small>
+`
+
 function loadPage(page) {
-    document.querySelector(".container").innerHTML = PAGES[page]
+    document.querySelector(".container").innerHTML = buttons + PAGES[page] + small
 }
 
 document.addEventListener("DOMContentLoaded", () => {
