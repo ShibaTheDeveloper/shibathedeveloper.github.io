40b3421c6b39eca868126e425c70aa4b00adfbc3
shiba <shibathedeveloper+github@gmail.com>
2025-12-16 21:18:21 +0100

    polish haha get the reference

--- js/router.js
+++ js/router.js
@@ -2,6 +2,8 @@ async function loadPage() {
   const page = location.hash.slice(1) || 'home';
   const html = await fetch(`pages/${page}.html`).then(r => r.text());
   document.getElementById('app').innerHTML = html;
+
+  document.title = page.charAt(0).toUpperCase() + page.slice(1) + "Page";
 }
 
 window.addEventListener('hashchange', loadPage);
