3e57d560582566c7d607a287656657a0de922045
shiba <shibathedeveloper+github@gmail.com>
2025-12-16 20:53:12 +0100

    Update router.js

--- js/router.js
+++ js/router.js
@@ -1,14 +1,7 @@
 async function loadPage() {
   const page = location.hash.slice(1) || 'home';
-
   const html = await fetch(`pages/${page}.html`).then(r => r.text());
   document.getElementById('app').innerHTML = html;
-
-  const ogTitle = document.querySelector('meta[property="og:title"]');
-  const baseTitle = "ShibaTheDeveloper's website!";
-
-  const formattedPage = page.charAt(0).toUpperCase() + page.slice(1);
-  ogTitle.setAttribute('content', `${baseTitle} | ${formattedPage}`);
 }
 
 window.addEventListener('hashchange', loadPage);
