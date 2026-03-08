dd2e649fe39842b1963a1d5e78866c8a18e25413
shiba <shibathedeveloper+github@gmail.com>
2026-03-08 19:49:37 +0100

    java update

--- js/javaUpdateEasterEgg.js
+++ js/javaUpdateEasterEgg.js
@@ -5,6 +5,11 @@ let clickable = true;
 if (javaLink) {
     const audio = new Audio("/assets/easter-egg/win10notif.ogg");
 
+    function cleanUpNotification() {
+        notificationImage.remove();
+        clickable = true
+    }
+
     function showNotification() {
         const notificationImage = document.createElement("img");
         notificationImage.src = "/assets/easter-egg/javaupdate.webp";
@@ -13,13 +18,20 @@ if (javaLink) {
         notificationImage.style.right = "-400px";
         notificationImage.style.boxShadow = "0 4px 10px rgba(0,0,0,0.3)";
         notificationImage.style.borderRadius = "6px";
-        notificationImage.style.transition = "right 0.5s ease-out";
+        notificationImage.style.transition = "right 0.2s ease-out";
 
         document.body.appendChild(notificationImage);
 
         setTimeout(() => {
             notificationImage.style.right = "20px";
         }, 20);
+
+        setTimeout(() => {
+            notificationImage.style.right = "-400px";
+            notificationImage.addEventListener(
+                "transitionend", cleanUpNotification, {once: true}
+            )
+        }, 3000);
     }
 
     function playSound() {
