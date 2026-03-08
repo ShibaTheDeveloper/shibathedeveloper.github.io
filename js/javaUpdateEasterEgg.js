95ca744e80a2f931130ed702c7850f6097955d66
shiba <shibathedeveloper+github@gmail.com>
2026-03-08 19:35:39 +0100

    teatgaytreyh

--- js/javaUpdateEasterEgg.js
+++ js/javaUpdateEasterEgg.js
@@ -10,19 +10,14 @@ if (javaLink) {
         notificationImage = document.createElement("img");
         notificationImage.src = "/assets/easter-egg/javaupdate.webp";
         notificationImage.style.position = "fixed";
-        notificationImage.style.bottom = "-100px";
-        notificationImage.style.right = "20px";
-        notificationImage.style.width = "300px";
-        notificationImage.style.boxShadow = "0 4px 10px rgba(0,0,0,0.3)";
-        notificationImage.style.borderRadius = "6px";
+        notificationImage.style.bottom = "20px";
+        notificationImage.style.right = "-100px";
         notificationImage.style.transition = "bottom 0.5s ease-out, opacity 0.5s ease-out";
-        notificationImage.style.opacity = "0";
 
         document.body.appendChild(notificationImage);
 
         requestAnimationFrame(() => {
-            notificationImage.style.bottom = "20px";
-            notificationImage.style.opacity = "1";
+            notificationImage.style.right = "20px";
         }, 10);
     }
 
