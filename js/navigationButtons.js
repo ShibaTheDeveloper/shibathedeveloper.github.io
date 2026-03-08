8011eddeeeca152b4ae22d8b647491e2f35fbaf4
shiba <shibathedeveloper+github@gmail.com>
2026-03-08 14:24:09 +0100

    buttons

---
+++ js/navigationButtons.js
@@ -0,0 +1,22 @@
+const navButtons = document.querySelectorAll('.button-container button');
+
+navButtons.forEach(button => {
+  button.addEventListener('click', () => {
+    const page = button.getAttribute('data-page');
+
+    location.hash = `#${page}`;
+
+    navButtons.forEach(btn => btn.id = '');
+    button.id = 'selected';
+  });
+});
+
+function updateSelectedButtonFromHash() {
+  const page = location.hash.slice(1) || 'home';
+  navButtons.forEach(btn => btn.id = '');
+  const currentBtn = Array.from(navButtons).find(btn => btn.getAttribute('data-page') === page);
+  if (currentBtn) currentBtn.id = 'selected';
+}
+
+window.addEventListener('hashchange', updateSelectedButtonFromHash);
+window.addEventListener('load', updateSelectedButtonFromHash);
